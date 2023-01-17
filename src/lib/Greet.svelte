f48d62b3bd6ed6e5bbaaed8e8861dfbf3b342f60
deadManAlive <alfarrakhan@gmail.com>
2023-01-17 23:19:55 +0700

    initial commit

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { invoke } from "@tauri-apps/api/tauri";
+
+    let name = "";
+    let greet_msg = "";
+
+    async function greet() {
+        greet_msg = await invoke('greet', { name });
+    }
+</script>
+
+<div>
+    <input id="greet-input" placeholder="Put your name here..." bind:value="{ name }" />
+    <button on:click="{ greet }">Greet</button>
+    <p>{ greet_msg }</p>
+</div>
